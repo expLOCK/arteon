84a966625bad29f528d66fa9ebb895f0b5063272
abdulaev_ii <abdulaev_ii@magnit.ru>
2023-06-01 00:08:48 +0000

    Add seeder factories

--- database/seeders/FavouriteSeeder.php
+++ database/seeders/FavouriteSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Favourite;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class FavouriteSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Favourite::factory(100)->create();
     }
 }
